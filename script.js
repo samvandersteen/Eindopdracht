a296bc4f516640ae58700411b30260a1830821db
samvandersteen <samvandersteen30@hotmail.com>
2015-10-13 15:22:15 +0200

    versie 1

---
+++ script.js
@@ -0,0 +1,17 @@
+var formProject = document.getElementById('project');
+var formStage = document.getElementById('stage');
+var radioProject = document.querySelector('input[name=submit-for][value=project]');
+var radioStage = document.querySelector('input[name=submit-for][value=stage]');
+
+formProject.classList.add('hide');
+formStage.classList.add('hide');
+
+radioProject.addEventListener('click', function(){
+    formProject.classList.remove('hide');
+    formStage.classList.add('hide');
+});
+
+radioStage.addEventListener('click', function(){
+    formProject.classList.add('hide');
+    formStage.classList.remove('hide');
+});
